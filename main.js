a390c19466a45f6c1330b3f8ed3a045661392407
vviillee <ville.munnukka@gmail.com>
2020-11-02 03:14:41 +0200

    push

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 
-document.getElementById("hae").addEventListener("click", loadData);
-
 const xmlFile = "data.xml";
 
-function loadData(){
-    var ajax = new XMLHttpRequest
-    ajax.onload = function(){
-        console.log(ajax.responseXML);
-        showData(this.responseXML, "viestit");   
-    }
-    ajax.open("GET", xmlFile, true);
-    ajax.send();
-
-}
 /**
- * 
- * @param {xmlObject} xmlData 
- * @param {string} targetUL 
+ * loadData - Hakee datan xml-tiedostosta
  */
-function showData(xmlData, targetUL){
+function loadData(){
 
+        var ajax = new XMLHttpRequest();
+        ajax.onload = function(){
+            alert('valmis');
+        }
+        ajax.open("GET", xmlFile, true)
+        ajax.send();
 }
