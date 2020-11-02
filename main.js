39b19717c8346e66e44de642965c28f784b92506
vviillee <ville.munnukka@gmail.com>
2020-11-02 04:20:20 +0200

    push

--- main.js
+++ main.js
@@ -7,11 +7,22 @@ const xmlFile = "data.xml";
  * loadData - Hakee datan xml-tiedostosta
  */
 function loadData(){
-
+        
         var ajax = new XMLHttpRequest();
         ajax.onload = function(){
-            alert('valmis');
+           console.log(ajax.responseXML);
+           showData( this.responseXML, "viestit" );
         }
-        ajax.open("GET", xmlFile, true)
+        ajax.open("GET", xmlFile, true);
         ajax.send();
+
+}
+/**
+ * showData - Näyttää annetun XML-datan valitussa UL-elementissä
+ * @param {xmlObject} xmlData 
+ * @param {string} targetUl 
+ */
+function showData(xmlData, targetUl){
+
+
 }
