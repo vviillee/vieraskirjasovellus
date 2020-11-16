e5abfd921e3b1b3ef1f4112440ae35813b2a6fdf
vviillee <ville.munnukka@gmail.com>
2020-11-16 11:40:41 +0200

    del.php

--- del.php
+++ del.php
@@ -2,3 +2,12 @@
 
 header("Content-type: application/json; charset-utf-8");
 
+if (!isset($_GET['index']) || !is_int(intaval($_GET['index']))) {
+    $response = array(
+        'status' => 'error',
+        'message' => 'No correct parameters'
+    );
+
+    echo json_encode($response);
+    die();
+}
