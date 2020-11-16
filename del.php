3a75ab8885eb931dd07dfd0bc2998148adb9416d
vviillee <ville.munnukka@gmail.com>
2020-11-16 11:48:24 +0200

    del.php

--- del.php
+++ del.php
@@ -11,3 +11,23 @@ if (!isset($_GET['index']) || !is_int(intaval($_GET['index']))) {
     echo json_encode($response);
     die();
 }
+
+$index = intval($_GET['index']);
+
+define("XML_FILE", "data.xml");
+$xml = simplexml_load_file(XML_FILE);
+unset($xml->viestit->viesti[$index]);
+
+$dom = new DOMDocument("1.0");
+$dom->preserveWhiteSpace = false;
+$dom->formatOutput = true;
+$dom->loadXML($xml->asXML());
+if ($dom->save(XML_FILE)) {
+    $response = array(
+        'status' => 'success',
+        'messages' => 'XML node deleted'
+    );
+
+    echo json_encode($response);
+
+}
